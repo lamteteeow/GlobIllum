07ea7951b166b9f5bd479a4e00733816b1aa8a44
Lam Tran <lamteteeow@gmail.com>
2025-06-07 00:10:10 +0200

    ex1 cleaned up unnecessary loops and comments

--- a01/src/algorithms/simple.cpp
+++ a01/src/algorithms/simple.cpp
@@ -24,15 +24,30 @@ struct SimpleRenderer : public Algorithm {
 
         // TODO ASSIGNMENT1
         // - add and initialize random samplers
-        HammersleySampler2D pixel_sampler;
+        //HammersleySampler2D pixel_sampler;
+        UniformSampler2D pixel_sampler;
         pixel_sampler.init(samples);
 		glm::vec2 pixel_sample;
 
-        HammersleySampler2D lens_sampler;
+        //HammersleySampler2D lens_sampler;
+        UniformSampler2D lens_sampler;
         lens_sampler.init(samples);
         glm::vec2 lens_sample;
 
+        //StratifiedSampler1D light_select_sampler{};
+        UniformSampler1D light_select_sampler{};                    
+        //HammersleySampler2D light_select_sampler{};
+        light_select_sampler.init(samples);
+        float light_select_sample;
+
+        //HammersleySampler2D light_sampler{};
+        UniformSampler2D light_sampler{};
+        light_sampler.init(samples);
+		glm::vec2 light_sample;
+
         // - apply supersampling over #samples and DOF using your samplers
+
+
         
         // Loop over samples
         for (uint32_t s = 0; s < samples; ++s) {
@@ -58,53 +73,15 @@ struct SimpleRenderer : public Algorithm {
                     // hint: use the following c++17 syntax to capture multiple return values:
                     // const auto [light_ptr, ignore_me] = scene.sample_light_source(...);
                     // auto [Li, shadow_ray, ignore_me2] = light_ptr->sample_Li(...);
-                    
-                    //StratifiedSampler1D light_select_sampler{};
-                    UniformSampler1D light_select_sampler{};                    uint32_t light_select_samples = scene.lights.size();
-                    constexpr uint32_t light_samples = 16;
-                    light_select_sampler.init(light_select_samples);
-                    for (uint32_t light_select_sample = 0; light_select_sample < light_select_samples; light_select_sample++) {
-                        const auto [light, light_pdf] = scene.sample_light_source(light_select_sampler.next());
-                        HammersleySampler2D light_sampler{};
-						//UniformSampler2D light_sampler{};
-                        light_sampler.init(light_samples);
-                        for (uint32_t light_sample = 0; light_sample < light_samples; light_sample++) {
-                            auto [Li, shadow_ray, dir_pdf] = light->sample_Li(hit.P, light_sampler.next());
-                            if (!scene.occluded(shadow_ray)) {
-                                L += Li * glm::max(glm::dot(shadow_ray.dir, hit.N), 0.f) * hit.albedo() / vec3(M_PI);
-                                //L += Li * glm::max(glm::dot(shadow_ray.dir, hit.N), 0.f) * hit.albedo() / vec3(M_PI) / (light_pdf * dir_pdf);
-                            }
-                        }
-                        //fbo.add_sample(x, y, L * (static_cast<AreaLight*>(light.get())->mesh.surface_area() / light_samples));
+
+					light_select_sample = light_select_sampler.next();
+                    const auto [light, light_pdf] = scene.sample_light_source(light_select_sample);
+                    //for (uint32_t light_sample = 0; light_sample < light_samples; light_sample++) {
+					light_sample = light_sampler.next();
+                    auto [Li, shadow_ray, dir_pdf] = light->sample_Li(hit.P, light_sample);
+                    if (!scene.occluded(shadow_ray)) {
+                        L += Li * glm::max(glm::dot(shadow_ray.dir, hit.N), 0.f) * hit.albedo();
                     }
-                    //continue;
-                    //L = hit.albedo();
-
-                    //// Sample light source randomly
-                    //StratifiedSampler1D light_select_sampler{};
-                    //uint32_t light_select_samples = scene.lights.size();
-                    //constexpr uint32_t light_samples = 16;
-                    //light_select_sampler.init(light_select_samples);
-
-                    //const auto [light_ptr, light_pdf] = scene.sample_light_source(light_select_sampler.next());
-
-                    //if (light_ptr && light_pdf > 0.0f) {
-                    //    // Sample a point on the light
-                    //    StratifiedSampler2D point_sampler;
-                    //    point_sampler.init(1); // One sample per light
-
-                    //    auto [Li, shadow_ray, dir_pdf] = light_ptr->sample_Li(hit.P, point_sampler.next());
-
-                    //    // Check visibility
-                    //    if (!scene.occluded(shadow_ray)) {
-                    //        // Calculate shading with BRDF
-                    //        float cos_theta = glm::max(glm::dot(shadow_ray.dir, hit.N), 0.f);
-                    //        vec3 brdf = hit.albedo() / vec3(M_PI); // Lambertian BRDF
-
-                    //        // Rendering equation (Monte Carlo estimator)
-                    //        L += brdf * Li * cos_theta / (light_pdf * dir_pdf);
-                    //    }
-                    //}
                 }
             }
             else // ray escaped the scene
